091949ed85b0663f22baa3d02262520c08b5700f
trymorez <78213565+trymorez@users.noreply.github.com>
2025-05-13 23:46:53 +0900

    Update Easing.cs

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Easing : MonoBehaviour
 {
+    public Image circle, circleOutline;
+    public Slider slider;
 
     void Start()
     {
-        
+        //circleOutline.DOColor(RandomColor(), 1.5f).SetEase(Ease.Linear).Pause();
+        circleOutline.DOFillAmount(0, 1.5f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
+            .OnStepComplete(() => {
+                circleOutline.fillClockwise = !circleOutline.fillClockwise;
+                circleOutline.DOColor(RandomColor(), 1.5f).SetEase(Ease.Linear);
+            })
+            .Pause();
+
+        StartTweens();
+    }
+
+    public void StartTweens()
+    {
+        DOTween.PlayAll();
     }
 
     void Update()
     {
         
+    }
+
+    Color RandomColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
     }
 }
