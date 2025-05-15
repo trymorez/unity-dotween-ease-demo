7d78851ab4c923d70d1795c41f9aef0eed38ed76
trymorez <78213565+trymorez@users.noreply.github.com>
2025-05-15 23:57:03 +0900

    Update Easing.cs

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -6,24 +6,25 @@ public class Easing : MonoBehaviour
 {
     public Image circle, circleOutline;
     public Slider slider;
+    [SerializeField] float easeTime = 0.5f;
 
     void Start()
     {
-        circleOutline.DOColor(RandomColor(), 0.5f).SetEase(Ease.Linear).Pause();
-        circleOutline.DOFillAmount(0, 0.5f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
+        circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear).Pause();
+        circleOutline.DOFillAmount(0, easeTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
             .OnStepComplete(() =>
             {
                 circleOutline.fillClockwise = !circleOutline.fillClockwise;
-                circleOutline.DOColor(RandomColor(), 1.5f).SetEase(Ease.Linear);
+                circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
             })
             .Pause();
 
-        circle.DOColor(RandomColor(), 0.5f).SetEase(Ease.Linear).Pause();
-        circle.DOFillAmount(0, 0.5f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
+        circle.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear).Pause();
+        circle.DOFillAmount(0, easeTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
             .OnStepComplete(() =>
             {
                 circleOutline.fillClockwise = !circleOutline.fillClockwise;
-                circleOutline.DOColor(RandomColor(), 1.5f).SetEase(Ease.Linear);
+                circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
             })
             .Pause();
 
