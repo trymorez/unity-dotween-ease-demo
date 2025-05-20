06cc7b8e94fb0414e6a8347287026ddbcb76a1c8
trymorez <78213565+trymorez@users.noreply.github.com>
2025-05-20 23:18:18 +0900

    Update Easing.cs

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -10,27 +10,39 @@ public class Easing : MonoBehaviour
 
     void Start()
     {
-        Sequence sequence;
+        Sequence sequenceCircle;
+        Sequence sequenceSlider;
 
         //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear).Pause();
         circleOutline.DOFillAmount(0, easeTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
-            .OnStepComplete(() =>
-            {
-                circleOutline.fillClockwise = !circleOutline.fillClockwise;
-                //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
-            })
-            .Pause();
+        .OnStepComplete(() =>
+        {
+            circleOutline.fillClockwise = !circleOutline.fillClockwise;
+            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
+        })
+        .Pause();
 
-        sequence = DOTween.Sequence();
-        sequence.Append(circle.transform.DOScale(Vector3.zero, easeTime * 0.5f));
-        sequence.Append(circle.transform.DOScale(Vector3.one * 0.5f, easeTime * 0.5f));
-        sequence.SetLoops(-1, LoopType.Restart);
-        sequence.OnComplete(() =>
-            {
-                //circle.fillClockwise = !circle.fillClockwise;
-                //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
-            })
-            .Pause();
+        sequenceCircle = DOTween.Sequence();
+        sequenceCircle.Append(circle.transform.DOScale(Vector3.zero, easeTime * 0.5f));
+        sequenceCircle.Append(circle.transform.DOScale(Vector3.one * 0.5f, easeTime * 0.5f));
+        sequenceCircle.SetLoops(-1, LoopType.Restart);
+        sequenceCircle.OnComplete(() =>
+        {
+            //circle.fillClockwise = !circle.fillClockwise;
+            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
+        })
+        .Pause();
+
+        sequenceSlider = DOTween.Sequence();
+        sequenceSlider.Append(slider.DOValue(1f, easeTime * 0.5f));
+        sequenceSlider.Append(slider.DOValue(0f, easeTime * 0.5f));
+        sequenceSlider.SetLoops(-1, LoopType.Restart);
+        sequenceSlider.OnComplete(() =>
+        {
+            //circle.fillClockwise = !circle.fillClockwise;
+            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
+        })
+        .Pause();
 
         StartTweens();
     }
