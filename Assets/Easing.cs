ee3e4012eda52122d2111be2af533a0deab63849
trymorez <78213565+trymorez@users.noreply.github.com>
2025-05-24 22:55:46 +0900

    V 0.99

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -8,6 +8,7 @@ public class Easing : MonoBehaviour
     public Slider slider;
     [SerializeField] float easeTime = 1.5f;
     float HalfEaseTime;
+    Ease ease = Ease.Linear;
 
     void Start()
     {
@@ -16,7 +17,7 @@ public class Easing : MonoBehaviour
         HalfEaseTime = easeTime * 0.5f;
 
         //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear).Pause();
-        circleOutline.DOFillAmount(0, easeTime).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo)
+        circleOutline.DOFillAmount(0, easeTime).SetEase(ease).SetLoops(-1, LoopType.Yoyo)
         .OnStepComplete(() =>
         {
             circleOutline.fillClockwise = !circleOutline.fillClockwise;
@@ -25,7 +26,7 @@ public class Easing : MonoBehaviour
         .Pause();
 
         sequenceCircle = DOTween.Sequence();
-        sequenceCircle.Append(circle.transform.DOScale(Vector3.zero, HalfEaseTime).SetEase(Ease.Linear));
+        sequenceCircle.Append(circle.transform.DOScale(Vector3.zero, HalfEaseTime).SetEase(ease));
         //sequenceCircle.Append(circle.transform.DOScale(Vector3.one * 0.5f, HalfEaseTime));
         sequenceCircle.SetLoops(-1, LoopType.Yoyo);
         sequenceCircle.OnComplete(() =>
@@ -36,7 +37,7 @@ public class Easing : MonoBehaviour
         .Pause();
 
         sequenceSlider = DOTween.Sequence();
-        sequenceSlider.Append(slider.DOValue(1f, HalfEaseTime).SetEase(Ease.Linear));
+        sequenceSlider.Append(slider.DOValue(1f, HalfEaseTime).SetEase(ease));
         //sequenceSlider.Append(slider.DOValue(0f, HalfEaseTime));
         sequenceSlider.SetLoops(-1, LoopType.Yoyo);
         sequenceSlider.OnComplete(() =>
