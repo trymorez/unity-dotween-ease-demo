4602e0ab89db2ea4da5235a3e791090ada7ee204
trymorez <78213565+trymorez@users.noreply.github.com>
2025-05-27 22:35:04 +0900

    Update Easing.cs

--- Assets/Easing.cs
+++ Assets/Easing.cs
@@ -18,34 +18,26 @@ public class Easing : MonoBehaviour
         Sequence sequenceSlider;
         HalfEaseTime = easeTime * 0.5f;
 
-        //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear).Pause();
         circleOutline.DOFillAmount(0, easeTime).SetEase(ease).SetLoops(-1, LoopType.Yoyo)
         .OnStepComplete(() =>
         {
             circleOutline.fillClockwise = !circleOutline.fillClockwise;
-            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
         })
         .Pause();
 
         sequenceCircle = DOTween.Sequence();
         sequenceCircle.Append(circle.transform.DOScale(Vector3.zero, HalfEaseTime).SetEase(ease));
-        //sequenceCircle.Append(circle.transform.DOScale(Vector3.one * 0.5f, HalfEaseTime));
         sequenceCircle.SetLoops(-1, LoopType.Yoyo);
         sequenceCircle.OnComplete(() =>
         {
-            //circle.fillClockwise = !circle.fillClockwise;
-            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
         })
         .Pause();
 
         sequenceSlider = DOTween.Sequence();
         sequenceSlider.Append(slider.DOValue(1f, HalfEaseTime).SetEase(ease));
-        //sequenceSlider.Append(slider.DOValue(0f, HalfEaseTime));
         sequenceSlider.SetLoops(-1, LoopType.Yoyo);
         sequenceSlider.OnComplete(() =>
         {
-            //circle.fillClockwise = !circle.fillClockwise;
-            //circleOutline.DOColor(RandomColor(), easeTime).SetEase(Ease.Linear);
         })
         .Pause();
 
